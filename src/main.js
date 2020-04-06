8492c7ecc56edd24d7ee21336353db40eda4fb50
Chris K <ikim1991@gmail.com>
2020-04-06 17:06:31 -0400

    Adding Web Page Routes

--- src/main.js
+++ src/main.js
@@ -26,3 +26,26 @@ dropMenu.addEventListener("mouseout", hideDropDown);
 function buttonFunction() {
   document.location.href = "./about.html"
 }
+
+// NavBar routing functionality
+const about = document.querySelector(".about");
+const resume = document.querySelector(".resume");
+const projects = document.querySelector(".projects");
+const contact = document.querySelector(".contact");
+
+function pageRoute(event){
+  if(event.target.classList.contains("about") && !document.location.href.includes("about")){
+    document.location.href = "./about.html"
+  } else if(event.target.classList.contains("resume") && !document.location.href.includes("resume")){
+    document.location.href = "./resume.html"
+  } else if(event.target.classList.contains("projects") && !document.location.href.includes("projects")){
+    document.location.href = "./projects.html"
+  } else if(event.target.classList.contains("contact") && !document.location.href.includes("contact")){
+    document.location.href = "./contact.html"
+  }
+}
+
+about.addEventListener("click", pageRoute);
+resume.addEventListener("click", pageRoute);
+projects.addEventListener("click", pageRoute);
+contact.addEventListener("click", pageRoute);
