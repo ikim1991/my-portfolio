bf1c2b08104a1edd629d4aedfba65956c0bed6a3
Chris K <ikim1991@gmail.com>
2020-04-06 18:09:11 -0400

    Adding Headers and Title to pages

--- src/main.js
+++ src/main.js
@@ -1,26 +1,8 @@
 // Adding Dropdown Functionality on the main index.html page
-
 const button = document.querySelector("#myButton");
 const dropMenu = document.querySelector("#dropBar");
 
-function collapseDropDown() {
-  if(dropMenu.className.indexOf("w3-show") == -1){
-    dropMenu.className += " w3-show";
-    button.className = button.className.replace(" fa fa-bars", " fa fa-close");
-  }
-}
 
-function hideDropDown(){
-  if(dropMenu.className.indexOf("w3-show") != -1){
-    dropMenu.className = dropMenu.className.replace(" w3-show", "");
-    button.className = button.className.replace(" fa fa-close", " fa fa-bars");
-  }
-}
-
-button.addEventListener("mouseover", collapseDropDown);
-button.addEventListener("mouseout", hideDropDown);
-dropMenu.addEventListener("mouseover", collapseDropDown);
-dropMenu.addEventListener("mouseout", hideDropDown);
 
 // Adding functionality to the "Learn More" button
 function buttonFunction() {
